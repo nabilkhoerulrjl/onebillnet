910afb71c062c93a8ecdb5a8d323cb62bc0f9abb
nabilkhoerulrjl2002 <nabilkhoerulrjl2002@gmail.com>
2023-12-23 19:06:49 +0700

    perbaikan chamel case name contactgroup table

--- application/models/ContactGroup_Model.php
+++ application/models/ContactGroup_Model.php
@@ -9,32 +9,32 @@ class ContactGroup_Model extends CI_Model {
 
     // Fungsi untuk menyimpan data grup kontak ke dalam database
     public function insert_contactgroup($data) {
-        $this->db->insert('contactgroup', $data);
+        $this->db->insert('ContactGroup', $data);
         return $this->db->insert_id();
     }
 
     // Fungsi untuk mendapatkan data grup kontak berdasarkan ID
-    public function get_contactgroup_by_id($id) {
+    public function get_contactgroup_by_id($id) { 
         $this->db->where('Id', $id);
-        $query = $this->db->get('contactgroup');
+        $query = $this->db->get('ContactGroup');
         return $query->row();
     }
 
     // Fungsi untuk mendapatkan semua data grup kontak
     public function getAllContactGroups() {
-        $query = $this->db->get('contactgroup');
+        $query = $this->db->get('ContactGroup');
         return $query->result();
     }
 
     // Fungsi untuk mengupdate data grup kontak berdasarkan ID
     public function update_contactgroup($id, $data) {
         $this->db->where('Id', $id);
-        $this->db->update('contactgroup', $data);
+        $this->db->update('ContactGroup', $data);
     }
 
     // Fungsi untuk menghapus data grup kontak berdasarkan ID
     public function delete_contactgroup($id) {
         $this->db->where('Id', $id);
-        $this->db->delete('contactgroup');
+        $this->db->delete('ContactGroup');
     }
 }
