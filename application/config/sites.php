f13f6f93f17485f1cffafbe20343d9d11000d5f3
nabilkhoerulrjl2002 <nabilkhoerulrjl2002@gmail.com>
2023-12-25 16:11:10 +0700

    Perubahan base url env staging

--- application/config/sites.php
+++ application/config/sites.php
@@ -2,5 +2,6 @@
     return [
         'localhost' => '.env.local',
         'web.localserver.site' => '.env.staging',
+        'onebillnet.localserver.site' => '.env.staging',
         // tambahkan situs lain di sini
     ];
